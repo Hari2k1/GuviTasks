74250911dc50d16634375e0713605c55ac184ea1
Hari2k1 <81380692+Hari2k1@users.noreply.github.com>
2022-02-08 17:08:57 +0530

    Update print_data.php

--- print_data.php
+++ print_data.php
@@ -1,5 +1,64 @@
-<?php
 
+<?php
+	//insert records to a json file
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                    
+        function get_data() {
+        $uname=$_POST['username'];
+        $email=$_POST['email'];
+        $pwd=$_POST['password'];
+        $dob=$_POST['dob'];
+        $contact=$_POST['contact'];
+        $age=$_POST['age'];
+    
+            $file_name='UserDetails'. '.json';
+    
+            if(file_exists("$file_name")) {
+                $current_data=file_get_contents("$file_name");
+                $array_data=json_decode($current_data, true);
+                                
+                $extra=array(
+                    'Username' => $_POST['username'],
+                    'Email' => $_POST['email'],
+                    'Password' => $_POST['password'],
+                    'DoB' => $_POST['dob'],
+                    'Contact' => $_POST['contact'],
+                    'Age' => $_POST['age'],
+                );
+                $array_data[]=$extra;
+                //echo "file exist<br/>";
+                return json_encode($array_data);
+            }
+            else {
+                $datae=array();
+                $datae[]=array(
+                    'Username' => $_POST['username'],
+                    'Email' => $_POST['email'],
+                    'Password' => $_POST['password'],
+                    'DoB' => $_POST['dob'],
+                    'Contact' => $_POST['contact'],
+                    'Age' => $_POST['age'],
+                );
+                return json_encode($datae);
+            }
+        }
+    
+        $file_name='UserDetails'. '.json';
+        
+        if(file_put_contents("$file_name", get_data())) {
+        //echo "<script>alert('success')</script>";	
+        $f=0;			
+        
+        }				
+        else {
+        //echo "<script>alert('There is some error')</script>";		
+        $f=1;		
+        }
+    }
+        
+    ?>
+
+<?php
 require 'D:/xampp/htdocs/redis/predis/src/Autoloader.php';
 Predis\Autoloader::register();
 $redis= new Predis\Client([
@@ -7,39 +66,46 @@ $redis= new Predis\Client([
     'host' =>'127.0.0.1',
     'port' =>6379,
 ]);
-$data_cache=$redis->get('users');
+$data_cache=$redis->lrange('user_insert',0,-1);
 
-if($data_cache)
-{
-    //display result
-    echo "From Redis Cache";
-    echo $data_cache;
-}
-else{
-    //connect db and create key ,cache key
 try{
-
-$mongo = new MongoDB\Driver\Manager("mongodb://127.0.0.1:27017");
-$filter  = [];
-$options = [];
-#constructing the querry
-$query = new MongoDB\Driver\Query($filter, $options);
-#executing
-$cursor = $mongo->executeQuery('db.users', $query);
-//echo "dumping results<br>";
-$temp='';
-foreach ($cursor as $document) 
-{   
-    $document = json_decode(json_encode($document),true);
-    echo $document['username']."\n".$document['email']."\n".$document['password']."\n".$document['age']."\n".$document['dob']."\n".$document['contact'];
-    $temp.= $document['username'].'  '.$document['email'].'  '.$document['password'].'  '.$document['age'].'  '.$document['dob'].'  '.$document['contact']. '<br>';
+    if($_POST && !empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['contact']) && !empty($_POST['dob']) && !empty($_POST['age']) ) 
+    {
+    //validate fields
+    $ins= array(
+        'username' => $_POST['username'],
+        'email' => $_POST['email'], 
+        'password' => $_POST['password'],
+        'contact' => $_POST['contact'],
+        'dob' => $_POST['dob'],
+        'age' => $_POST['age'],
+    );
+    
+    if($data_cache)
+    {
+        //$temp=json_decode($data_cache,true);
+        //$temp[]=$ins;
+        $redis->lpush('user_insert',json_encode($ins));
+        print_r("cache exists and data saved in cache");
+    }
+    else
+    {   
+        $temp=$ins;
+        $redis->lpush('user_insert',json_encode($temp));
+    }
+    //database operation
+    //we can also separately use insert operation to insert records from cache to the db server and then erase the cache, regularly
+    $mongo = new MongoDB\Driver\Manager("mongodb://127.0.0.1:27017");
+    $bulkWrite = new MongoDB\Driver\BulkWrite;
+    $bulkWrite->insert($ins);
+    $mongo->executeBulkWrite('db.users', $bulkWrite);
 }
-$redis->set('users',$temp);
-$redis->expire('users',10); //keys will expire after 10secs , in order to avoid stale data
+else
+{
+    print_r("none to store");
+}
 }
 catch(MongoConnectionException $e){
     var_dump($e);
-}
-
 }
 ?>
